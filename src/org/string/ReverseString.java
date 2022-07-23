ff0de6914632598c9e4a3e0c5006f34349f17fad
surey21 <sk202243@gmail.com>
2022-07-23 17:12:41 +0530

    new code

---
+++ src/org/string/ReverseString.java
@@ -0,0 +1,20 @@
+package org.string;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+//		String s = "Suresh";
+//		String rev ="";
+//		for (int i = 0; i < s.length()-1;i--) {
+//			char c = s.charAt(i);
+//			rev = rev+c;
+//			
+//		}
+//	System.out.println(rev);
+//	}
+
+		
+		
+		
+}
+}
--- src/org/string/StringClass.java
+++ src/org/string/StringClass.java
@@ -63,6 +63,7 @@ public class StringClass {
 		
 		int compareTo = a.compareTo(a1);
 		System.out.println(compareTo);
+		System.out.println(substring2);
 		
 		
 		
